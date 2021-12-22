438585638a51bf0f193fa030ab988f86bef20464
felisuco069 <70918916+felisuco069@users.noreply.github.com>
2021-12-22 09:09:31 +0100

    Estilado de tabla

--- member list org/src/component/members-table-row.tsx
+++ member list org/src/component/members-table-row.tsx
@@ -1,21 +1,23 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
 import { generatePath, Link } from "react-router-dom";
+import { StyledTableCell, StyledTableRow } from "../layaut/table-layaut";
 
 export const GetMemberTableRow = (props) => {
   const { member } = props;
   return (
-    <tr key={member.id}>
-      <td>
-        <img src={member.avatar_url} style={{ width: "5rem" }} />
-      </td>
-      <td>
+    <StyledTableRow key={member.id}>
+      <StyledTableCell>
+        <Avatar src={member.avatar_url} style={{ width: "5rem" }} />
+      </StyledTableCell>
+      <StyledTableCell align="center">
         <span>{member.id}</span>
-      </td>
-      <td>
+      </StyledTableCell>
+      <StyledTableCell align="center">
         <Link to={generatePath("/detail/:id", { id: member.login })}>
           {member.login}
         </Link>
-      </td>
-    </tr>
+      </StyledTableCell>
+    </StyledTableRow>
   );
 };
--- member list org/src/list.tsx
+++ member list org/src/list.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Button, TextField } from "@material-ui/core";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 import { getMembers } from "./api";
 import { GetMemberTableRow } from "./component/members-table-row";
 import { MyContext } from "./core/myContext";
+import { StyledTableCell, useStyles } from "./layaut/table-layaut";
 
 export const ListPage: React.FC = () => {
   const { inputValue, setInputValue, members, setMembers } =
@@ -18,29 +26,40 @@ export const ListPage: React.FC = () => {
       setMembers(data);
     });
   };
-
+  const classes = useStyles();
   return (
     <>
       <h2>Hello from List page</h2>
-      <input
-        onChange={(e) => setInputValue(e.target.value)}
-        value={inputValue}
-      ></input>
-      <button onClick={handleClick}>Search</button>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Avatar</th>
-            <th>Id</th>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {members.map((member) => (
-            <GetMemberTableRow key={member.id} member={member} />
-          ))}
-        </tbody>
-      </table>
+      <div>
+        <TextField
+          size="small"
+          id="outlined-search"
+          label="Search field"
+          type="search"
+          variant="outlined"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+        <Button variant="contained" color="primary" onClick={handleClick}>
+          Search
+        </Button>
+      </div>
+      <TableContainer component={Paper} className="table">
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Avatar</StyledTableCell>
+              <StyledTableCell align="center">Id</StyledTableCell>
+              <StyledTableCell align="center">Name</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {members.map((member) => (
+              <GetMemberTableRow key={member.id} member={member} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
